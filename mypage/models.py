2fb8789f8d7ff3bd9b77732908c8245e48687aa2
leejongsung <time8970@naver.com>
2019-03-16 22:54:02 +0900

    change mypage/models.py comment

--- mypage/models.py
+++ mypage/models.py
@@ -1,23 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.conf import settings 
-from django.utils import timezone
+
+#from django.utils import timezone
 from django.urls import reverse
 
 class MyPage(models.Model):
     """
-    name: UserName
-    age: UserAge
-    high_school_name : 고등학교 이름
-    high_adm_date : 고등학교 입학일
-    high_grad_date : 고등학교 졸업일
+    user_name = user의 id
+
+    name = 이름
+    age = 나이
     
-    univ_name : 대학 이름
-    major_name : 전공 이름
-    sub_major_name : 부전공 이름
-    univ_adm_date : 대학 입학일
-    univ_grad_date : 대학 졸업일
+    high_school_name = 고등학교 이름
+    high_adm_date = 고등학교 입학일
+    high_grad_date = 고등학교 졸업일
+    
+    univ_name = 대학교 이름
+    major_name = 주전공 이름
+    sub_major_name = 복수, 부전공 이름
+    univ_adm_date = 대학교 입학일
+    univ_grad_date = 대학교 졸업일
     """
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=30, default='0')
 
     name = models.CharField(max_length=10)
     age = models.PositiveIntegerField(default=0)
@@ -36,12 +41,18 @@ class MyPage(models.Model):
         return "id : {}, name : {}, age : {}".format(self.id, self.name, self.age)
 
 class Question(models.Model):
+    """
+    myPage = mypage
+    question_title = 질문 제목
+    question_text = 답변 내용
+    #question_text_file = 답변 내용을 저장할 파일
+    question_text_num = 답변 길이 제한
+    """
     myPage = models.ForeignKey(MyPage, on_delete=models.CASCADE)
-    user_id = models.CharField()
     question_title = models.CharField(max_length=200)
     question_text = models.TextField()
     #question_text_file = models.FileField(upload_to="question_log/{}".format(self.id))
-    question_text_num = models.PositiveIntegerField()
+    question_text_num = models.PositiveIntegerField(default=2000)
 
     def __str__(self):
         return self.question_title
@@ -50,7 +61,13 @@ class Question(models.Model):
         return reverse('mypage:detail', args=[str(self.id)])
 
 class Log(models.Model):
+    """
+    question = question
+    save_date = 저장일
+    log_message = 저장 메시지
+    log_text = 저장 내용
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     save_date = models.DateTimeField()
-    log_message = models.CharField()
+    log_message = models.CharField(max_length=20)
     log_text = models.TextField()
